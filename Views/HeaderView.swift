3a2ca044282ae07fb73dd3e70e5b3f93d7fb0b36
Nursat2022 <nursatsakyshev2003@gmail.com>
2024-05-21 20:53:13 +0500

    main-header

---
+++ Views/HeaderView.swift
@@ -0,0 +1,30 @@
+//
+//  HeaderView.swift
+//  arbuz-ios-task
+//
+//  Created by Nursat Sakyshev on 21.05.2024.
+//
+
+import SwiftUI
+
+struct HeaderView: View {
+    var product: Product
+    var body: some View {
+        GeometryReader { geomety in
+            product.image
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(
+                    width: geomety.size.width,
+                    height: geomety.size.height
+                )
+                .offset(y: -geomety.frame(in: .global).minY * 0.8)
+        }
+        .onTapGesture {
+        }
+    }
+}
+
+#Preview {
+    HeaderView(product: products[8])
+}
