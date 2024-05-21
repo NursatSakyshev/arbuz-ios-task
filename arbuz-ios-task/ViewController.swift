4df3d73d6d3ea6904066c23435a2c6206b08ee19
Nursat2022 <nursatsakyshev2003@gmail.com>
2024-05-21 20:03:32 +0500

    Initial commit

---
+++ arbuz-ios-task/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  arbuz-ios-task
+//
+//  Created by Nursat Sakyshev on 21.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+}
+
